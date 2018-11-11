0c8b52a783b113b5f388ad7c9a58557fb4c62edb
Arnab Gupta <arnab.gupta@quikr.com>
2018-11-11 23:02:17 +0530

    controller get and post request working

--- controllerofgit/src/controller.js
+++ controllerofgit/src/controller.js
@@ -7,7 +7,24 @@ app.use(cors());
 
 app.get('/', (request, response) => {
 	response.send('hello from controller');
-	console.log('hello from controller');
+	//console.log('hello from controller');
+});
+
+app.get('/testgetquery', (request, response) => {
+	const {name, price} = request.query;
+	if(name==null||price==null){
+		response.send('error fields empty');
+	}
+	response.send(name + price);
+});
+
+app.post('/testpostquery', (request, response) => {
+	const email = request.param('email');
+	const password =  request.param('password');
+	if(email==null||password==null){
+		response.send('post response is not working now');
+	}
+	response.send('post response is working now '+email+' '+password);
 });
 
 app.listen(4000, () => {
